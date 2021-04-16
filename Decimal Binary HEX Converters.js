ee558917bc443a12d789d45107903291c66cea9c
CallMeOli <81488914+CallMeOli@users.noreply.github.com>
2021-04-16 15:28:54 +0100

    Binary & Hexadecimal support added

--- Decimal Binary HEX Converters.js
+++ Decimal Binary HEX Converters.js
@@ -1,5 +1,5 @@
 // Binary & Denary
-const denaryToBinary = (num, split = true) => {
+const denaryToBinary = (num, split) => {
     // Convert to binary
     /*
     Denary: 11
@@ -117,4 +117,84 @@ const hexToDenary = hex => {
         sum += numbers[i] * 16**i
     }
     return sum;
+};
+
+// Hex & Binary
+const hexToBinary = (hex, split) => {
+    const translations = {
+        'A': 10,
+        'B': 11,
+        'C': 12,
+        'D': 13,
+        'E': 14,
+        'F': 15
+    };
+    const numbers = hex.split('');
+    // Convert each hexadecimal digit to binary
+    for (i in numbers) {
+        let num = numbers[i];
+        if (isNaN(num)) {
+            num = translations[num];
+        };
+        let quotient = Math.floor(num / 2);
+        let remainder = num % 2;
+        let remainders = [remainder];
+        while (quotient >= 1) {
+            let prevQuotient = quotient;
+            quotient = Math.floor(prevQuotient / 2);
+            remainder = prevQuotient % 2;
+            remainders.push(remainder);
+        };
+        // Reverse the remainders array and join the values into a string
+        remainders = remainders.reverse().join('');
+        // Add 0's
+        remainders = '0'.repeat(4 - remainders.split(' ')[0].length) + remainders;
+        numbers[i] = remainders;
+    }
+    // Split binary [ 01010101 -> 0101 0101 ]
+    if (split) {
+        for (i in numbers) {
+            if (i % 4 == 0) {
+                numbers[i] = numbers[i] + ' ';
+            };
+        };
+    };
+    return numbers.join('');
+};
+
+const binaryToHex = binary => {
+    // Reverse binary (string or number) as a string
+    binary = binary.toString().split('').reverse().join('');
+    // Convert to denary
+    let denary = 0;
+    for (i in binary) {
+        denary += binary[i] * 2**i;
+    };
+    // Convert to hex
+    let quotient = Math.floor(denary / 16);
+    let remainder = denary % 16;
+    let remainders = [remainder];
+    while (quotient >= 1) {
+        let prevQuotient = quotient;
+        quotient = Math.floor(prevQuotient / 16);
+        remainder = prevQuotient % 16;
+        remainders.push(remainder);
+    };
+    // Convert remainders to hexadecimal values
+    const translations = {
+        10: 'A',
+        11: 'B',
+        12: 'C',
+        13: 'D',
+        14: 'E',
+        15: 'F'
+    };
+    for (i in remainders) {
+        let denary = remainders[i];
+        if (denary > 9) {
+            remainders[i] = translations[denary];
+        };
+    };
+    // Reverse the remainders array and join the values into a string
+    return remainders.reverse().join('');
 };
